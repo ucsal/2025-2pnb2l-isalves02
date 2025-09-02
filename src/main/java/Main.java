d6d44ffd393986017f9e1dddfeaa2a121b50b909
Isabel Alves <66089395+isalves02@users.noreply.github.com>
2025-09-02 12:43:42 +0000

    Correçãopara atender o princípio de substituição de Liskov

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,20 @@
+// import br.com.mariojp.solid.lspaccounts.BankService;
 import br.com.mariojp.solid.lspaccounts.BankService;
 import br.com.mariojp.solid.lspaccounts.CheckingAccount;
 import br.com.mariojp.solid.lspaccounts.SavingsAccount;
 
 public class Main {
     public static void main(String[] args) {
+        BankService bankService = new BankService();
         CheckingAccount checking = new CheckingAccount();
         checking.deposit(100);
-        new BankService().processWithdrawal(checking, 30);
+        bankService.processCheckingWithdrawal(checking, 30);
         System.out.println("Checking balance: " + checking.getBalance()); // 70.0
 
         SavingsAccount savings = new SavingsAccount();
         savings.deposit(100);
-        // Estado inicial: irá lançar UnsupportedOperationException (ilustra o problema LSP).
-        new BankService().processWithdrawal(savings, 30);
+        // // Estado inicial: irá lançar UnsupportedOperationException (ilustra o problema LSP).
+        bankService.processSavingsWithdrawal(savings, 30);
         System.out.println("Savings balance: " + savings.getBalance());
     }
 }
